1e14a0535a298bc0f03789f3358215a1e8b28c6a
MuhammedGit <Muhammedalayyoubb@gmail.com>
2015-11-23 23:42:09 +0200

    Update ha1.sh

--- ha1.sh
+++ ha1.sh
@@ -2,12 +2,52 @@
 MIRROR=http://www.gtlib.gatech.edu/pub/apache/hadoop/common/hadoop-1.1.1/
 VERSION=hadoop-2.7.1
 
-apt-get update
-apt-get -y install openjdk-7-jdk
+
+
 
 #download hadoop, untar, put in /usr/local
-cd ~
-cd /Downloads
+
+cd ~/Downloads
 
 tar -xzf "$VERSION".tar.gz
 mv  $VERSION /usr/local
+
+#create user and group
+addgroup hadoop
+adduser --ingroup hadoop hduser
+echo user created........................................
+# app folder; who uses this ????
+mkdir -p /app/hadoop/tmp
+chown hduser:hadoop /app/hadoop/tmp
+chmod 755 /app/hadoop/tmp
+
+#modify hadoop-env
+cd /usr/local/$VERSION/etc/hadoop
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> hadoop-env.sh
+echo "export HADOOP_OPTS=-Djava.net.preferIPv4Stack=true" >> hadoop-env.sh
+
+#get configuration files
+rm core-site.xml
+wget https://raw.githubusercontent.com/MuhammedGit/MuhammedGit/Muho/conf/core-site.xml
+rm mapred-site.xml.template
+wget https://raw.githubusercontent.com/MuhammedGit/MuhammedGit/Muho/conf/mapred-site.xml.template
+rm hdfs-site.xml
+wget https://raw.githubusercontent.com/MuhammedGit/MuhammedGit/Muho/conf/hdfs-site.xml
+
+# chmod, symbolic links
+cd /usr/local
+ln -s $VERSION hadoop
+chown -R hduser:hadoop $VERSION
+echo .......ss baglantisi 
+su - hduser -c "echo | ssh-keygen -t rsa -P \"\""
+cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
+su - hduser -c "ssh -o StrictHostKeyChecking=no localhost echo "# login once, to add to known hosts
+su - hduser -c "/usr/local/hadoop/bin/hadoop namenode -format"
+su - hduser -c "echo | /usr/local/hadoop-2.7.1/sbin/start-all.sh"
+
+
+
+
+
+
+
