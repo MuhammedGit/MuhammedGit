40c9974fc2c4a177e440b37a25147e32ca4b77d3
MuhammedGit <Muhammedalayyoubb@gmail.com>
2015-11-23 19:54:36 +0200

    Create ha1.sh

---
+++ ha1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MIRROR=http://www.gtlib.gatech.edu/pub/apache/hadoop/common/hadoop-1.1.1/
+VERSION=hadoop-2.7.1
+
+apt-get update
+apt-get -y install openjdk-7-jdk
+
+#download hadoop, untar, put in /usr/local
+cd /Downloads
+
+tar -xzf "$VERSION".tar.gz
+mv  $VERSION /usr/local
