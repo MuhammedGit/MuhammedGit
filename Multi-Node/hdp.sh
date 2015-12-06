7a8f4367b344c573cd31e28e3c1b60f59e8d3d48
MuhammedGit <Muhammedalayyoubb@gmail.com>
2015-12-06 18:43:24 +0200

    Update hdp.sh

--- Multi-Node/hdp.sh
+++ Multi-Node/hdp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 VERSION=hadoop-2.7.1
+apt-get install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
+apt-get install ssh
 #download hadoop, untar, put in /usr/local
 cd ~/Downloads
 wget http://ftp.itu.edu.tr/Mirror/Apache/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz
@@ -10,17 +15,20 @@ mv  $VERSION /usr/local
 #create user and group
 addgroup hadoop
 adduser --ingroup hadoop hduser
+sudo adduser hduser sudo
 
 mkdir -p /app/hadoop/tmp
 chown hduser:hadoop /app/hadoop/tmp
 chmod 755 /app/hadoop/tmp
 
+mv $VERSION hadoop
+chown -R hduser:hadoop hadoop
 #modify hadoop-env
-cd /usr/local/$VERSION/etc/hadoop
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> hadoop-env.sh
-#echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> hadoop-env.sh
+cd /usr/local/hadoop/etc/hadoop
+#echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> hadoop-env.sh
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> hadoop-env.sh
 echo "export HADOOP_OPTS=-Djava.net.preferIPv4Stack=true" >> hadoop-env.sh#get configuration files
-
+m
 rm core-site.xml
 wget https://raw.githubusercontent.com/MuhammedGit/MuhammedGit/Muho/Multi-Node/conf/core-site.xml
 rm mapred-site.xml.template
@@ -29,3 +37,7 @@ rm hdfs-site.xml
 wget https://raw.githubusercontent.com/MuhammedGit/MuhammedGit/Muho/Multi-Node/conf/hdfs-site.xml
 rm yarn-site.xml
 wget https://raw.githubusercontent.com/MuhammedGit/MuhammedGit/Muho/Multi-Node/conf/yarn-site.xml
+
+echo $1 > /etc/hostname
+
+reboot
