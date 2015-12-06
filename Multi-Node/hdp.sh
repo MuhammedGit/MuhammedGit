1d136716f298b782a0653ab947869e942f1b5d4d
MuhammedGit <Muhammedalayyoubb@gmail.com>
2015-12-06 20:29:32 +0200

    Update hdp.sh

--- Multi-Node/hdp.sh
+++ Multi-Node/hdp.sh
@@ -40,5 +40,15 @@ rm yarn-site.xml
 wget https://raw.githubusercontent.com/MuhammedGit/MuhammedGit/Muho/Multi-Node/conf/yarn-site.xml
 
 echo $1 > /etc/hostname
+IP=$(hostname -I)
+
+echo "127.0.0.1		localhost" > ~/etc/hosts
+echo "$IP		$1" >> ~/etc/hosts
+echo "# The following lines are desirable for IPv6 capable hosts" >> ~/etc/hosts
+echo "::1     ip6-localhost ip6-loopback" >> ~/etc/hosts
+echo "fe00::0 ip6-localnet" >> ~/etc/hosts
+echo "ff00::0 ip6-mcastprefix" >> ~/etc/hosts
+echo "ff02::1 ip6-allnodes" >> ~/etc/hosts
+echo "ff02::2 ip6-allrouters" >> ~/etc/hosts
 
 reboot
